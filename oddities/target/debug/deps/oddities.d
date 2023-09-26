9fdf5955f9ab172c2083a36c80753455abd37cff
PERRIER <m1x6me@gmail.com>
2023-09-26 14:49:38 +0200

    Kattis - Oddities

---
+++ oddities/target/debug/deps/oddities.d
@@ -0,0 +1,5 @@
+C:\Users\m1x6m\IdeaProjects\RUST\oddities\target\debug\deps\oddities.exe: src\main.rs
+
+C:\Users\m1x6m\IdeaProjects\RUST\oddities\target\debug\deps\oddities.d: src\main.rs
+
+src\main.rs:
---
+++ oddities/target/debug/oddities.d
@@ -0,0 +1,1 @@
+C:\Users\m1x6m\IdeaProjects\RUST\oddities\target\debug\oddities.exe: C:\Users\m1x6m\IdeaProjects\RUST\oddities\src\main.rs
